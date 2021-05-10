3abcfca9c3be880a0741973c984707d98359615f
MrLoh <flojoto@hotmail.com>
2021-05-09 20:21:30 -0500

    broken blure view example

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {BlurView} from 'expo-blur'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <BlurView style={{
+        position: "absolute" , 
+        width: '100%', 
+        height: '100%', 
+        background: 'red'
+        }}></BlurView>
     </View>
   );
 }
